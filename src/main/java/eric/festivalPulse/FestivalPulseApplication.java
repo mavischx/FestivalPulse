073736b09f900a3c1b9a6692648c521edaacdecb
mavischx <mavis.chx@gmail.com>
2026-07-31 10:34:22 +0100

    first commit

---
+++ src/main/java/eric/festivalPulse/FestivalPulseApplication.java
@@ -0,0 +1,15 @@
+package eric.festivalPulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestivalPulseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FestivalPulseApplication.class, args);
+        System.out.println("Hello World");
+
+    }
+
+}
---
+++ src/test/java/eric/festivalPulse/FestivalPulseApplicationTests.java
@@ -0,0 +1,13 @@
+package eric.festivalPulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestivalPulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
